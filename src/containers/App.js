a1ce554e7e84ccda10ac4bbe26da34c3a333b7ac
TomaszMoroz <tmoroz688@gmail.com>
2017-12-23 14:52:10 +0100

    mapDispatchToProps foo

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.props.points}</h1>
+          <h1 className="App-title">{this.props.points.result}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -19,7 +19,22 @@ class App extends Component {
 }
 
 const mapStateToProps = (sate) => {
-  points: state.points
+  return {
+    points: state.points
+  };
+  
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      displayPoints:  () => {
+        dispatch({
+          type: 'HANDLECLICK',
+          payload: 1
+        });
+      }
+  };
+
 };
 
 export default App;
